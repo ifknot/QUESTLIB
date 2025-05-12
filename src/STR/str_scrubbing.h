a41d95865ebb1e1c08060c98615a646049204efe
ifknot <ifknot@hotmail.com>
2025-05-12 11:46:28 +0100

    Update str_scrubbing.h

--- src/STR/str_scrubbing.h
+++ src/STR/str_scrubbing.h
@@ -5,23 +5,35 @@
 
 #include "str_constants.h"
 
+/**
+* @brief remove from the zero terminated string any leading and trailing target characters and any contiguous duplicated target characters between words.
+*/
+uint16_t str_trim_character(char* string, const char target);
 
-uint16_t str_trim_character(char* zsource, char target);
+//uint16_t str_copy_trim_character(const char* source, char* destination, const char* targets_array);
 
-uint16_t str_trim_characters(char* zsource, char* zcharacters);
 
-//uint16_t str_copy_trim_characters(char* const zsource, char* zdestination, char* const zcharacters);
+/**
+* @brief convert any target characters in the zero terminated string to the first charcter in the target character array then trim that character from the string 
+*/
+uint16_t str_trim_characters(char* string, const char* targets);
 
-inline uint16_t str_trim_whitespace(char* zsource) {
-    return str_trim_characters(zsource, STR_WHITESPACE);
+//uint16_t str_copy_trim_characters(const char* const source, destination, const char* targets_array);
+
+/**
+* @brief convert all whitespace characters in STR_WHITESPACE to a space (0x20) and then trim leading and trailing spaces and any duplicate spaces between words
+* @note because the C isblank() function only recognises space (0x20) and horizontal tab (0x09) in the default locale it is necessary to use this more complete approach 
+*/
+inline uint16_t str_trim_whitespace(char* string) {
+    return str_trim_characters(string, STR_WHITESPACE);
 }
 
-//uint16_t str_copy_trim_whitespace(char* const zsource, char* zdestination);
+//uint16_t str_copy_trim_whitespace(const char* source, char* destination);
 
-//uint16_t str_remove_characters(char* zsource, char* const zcharacters);
+//uint16_t str_remove_characters(char* source, const char* targets);
 
-uint16_t str_remove_punctuation(char* zsource);
+uint16_t str_remove_punctuation(char* string);
 
-//uint16_t str_copy_remove_characters(char* const zsource, char* zdestination, char* const zcharacters);
+//uint16_t str_copy_remove_characters(const char* source, char* destination, const char* targets);
 
 #endif
