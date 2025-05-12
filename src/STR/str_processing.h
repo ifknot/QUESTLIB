3d5ec9f5896ce90d005a1d66a2eefeb1e56e89f8
ifknot <ifknot@hotmail.com>
2025-05-12 10:28:04 +0100

    Update str_processing.h

--- src/STR/str_processing.h
+++ src/STR/str_processing.h
@@ -5,8 +5,15 @@
 
 #include "str_types.h"
 
+/**
+* @brief count the number of distinct alpha-numeric locale specific "words" in a zero terminated string as separated by non-alpha-numeric character(s).
+*/
 uint16_t str_count_words(const char* string);
 
+/**
+* @brief copy distinct alpha-numeric locale specific "words" from a zero terminated string into an array of zero terminated strings.
+* @note no bounds checking for the size of string_array!
+*/
 uint16_t str_enumarate_words(const char* string, char** string_array);
 
 //uint16_t str_tokenize(char* string, char** dictionary, str_token_t* tokens);
