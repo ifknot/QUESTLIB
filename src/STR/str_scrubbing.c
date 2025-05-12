a38efcb09bfaeb72705e574d266c4dd9c5328c67
ifknot <ifknot@hotmail.com>
2025-05-12 11:51:32 +0100

    Update str_scrubbing.c

--- src/STR/str_scrubbing.c
+++ src/STR/str_scrubbing.c
@@ -5,50 +5,50 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-uint16_t str_trim_character(char* zsource, const char target) {
+uint16_t str_trim_character(char* string, const char target) {
     uint16_t i = 0; // search index;
     uint16_t j = 0; // copy index;
-    bool islead = (zsource[i++] == target) ? true : false; // leading target?
-    while(islead && zsource[i] == target && zsource[i++]); // skip them
-    while (zsource[i]) {
-        if(zsource[i] == target) {
-            zsource[j++] = zsource[i++]; // allow 1 copy
-            while(zsource[i] == target) { // skip any others
+    bool islead = (string[i++] == target) ? true : false; // leading target?
+    while(islead && string[i] == target && string[i++]); // skip them
+    while (string[i]) {
+        if(string[i] == target) {
+            string[j++] = string[i++]; // allow 1 copy
+            while(string[i] == target) { // skip any others
                 i++;
             };
         }
         else { // let the rest through
-            zsource[j++] = zsource[i++];
+            string[j++] = string[i++];
         }
     }
-    j = (zsource[j - 1] == target) ? j - 1 : j; // check for trailing target
-    zsource[j] = 0; // terminate the copied string
+    j = (string[j - 1] == target) ? j - 1 : j; // check for trailing target
+   string[j] = 0; // terminate the copied string
     return i - j;
 }
 
-uint16_t str_trim_characters(char* zsource, const char* ztargets) {
+uint16_t str_trim_characters(char* string, const char* targets) {
     uint16_t i = 0; // source index
-    while (ztargets[i]) { // convert all targets to target[0]
+    while (targets[i]) { // convert all targets to target[0]
        uint16_t j = 0;
-       while(zsource[j]) {
-           if(zsource[j] == ztargets[i]) {
-               zsource[j] = ztargets[0];
+       while(string[j]) {
+           if(string[j] == targets[i]) {
+               string[j] = targets[0];
            }
            j++;
        }
        i++;
     };
-    return str_trim_character(zsource, ztargets[0]);
+    return str_trim_character(string, targets[0]);
 }
 
-uint16_t str_remove_punctuation(char* zsource) {
+uint16_t str_remove_punctuation(char* string) {
     uint16_t i = 0; // search index;
     uint16_t j = 0; // copy index;
-    while(zsource[i]) {
-        if(ispunct(zsource[i])) {
+    while(string[i]) {
+        if(ispunct(string[i])) {
             i++;
         }
-        zsource[j++] = zsource[i++];
+        string[j++] = string[i++];
     }
     return i - j;
 }
