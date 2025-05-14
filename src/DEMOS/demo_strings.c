e7904b8ec068661c224585e7e34774852c125928
ifknot <ifknot@hotmail.com>
2025-05-14 10:31:07 +0100

    Update demo_strings.c

--- src/DEMOS/demo_strings.c
+++ src/DEMOS/demo_strings.c
@@ -12,7 +12,7 @@
 int demo_strings(int argc, char** argv) {
     char s[] = "  \n\tKill,  the\n   C3PO!     with   \ta    axe ! ?       ";
     char* list[] = {"1apple","2apple","3apple","4apple","5apple","6apple", 0};
-    str_word_token_t dict[] = {{"",0},{"LOOK", TOK_LOOK}, {"L", TOK_LOOK}, {"EXAMINE", TOK_EXAMINE}, {"X", TOK_EXAMINE}};
+    str_word_token_t dict[] = {{"",0},{"LOOK", TOK_LOOK}, {"L", TOK_LOOK}, {"EXAMINE", TOK_EXAMINE}, {"X", TOK_EXAMINE}, 0};
     str_token_t tokens[6];
     int i = 0; while(list[i]) printf("%s\n", list[i++]);
 
