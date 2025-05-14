0320a26ba8cc3ab24a96c627ae5b77bdb7fb4133
ifknot <ifknot@hotmail.com>
2025-05-14 10:38:48 +0100

    Update str_types.h

--- src/STR/str_types.h
+++ src/STR/str_types.h
@@ -3,11 +3,18 @@
 
 #include <stdint.h>
 
+#include "str_constants.h"
+
 typedef uint16_t str_token_t;
 
 typedef struct {
-    char word[15];
+    char word[STR_MAX_DICTIONARY_WORD_LENGTH];
     str_token_t token;
 } str_word_token_t;
+
+typdef struct {
+    str_word_token_t* first;
+    str_word_token_t* last;
+} str_token_dictionary_t; 
 
 #endif
