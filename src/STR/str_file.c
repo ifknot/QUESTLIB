e4876ea755eaadb10f5c951b5d096d6e0a213fbf
ifknot <ifknot@hotmail.com>
2025-05-18 11:22:03 +0100

    Update str_file.c

--- src/STR/str_file.c
+++ src/STR/str_file.c
@@ -2,13 +2,13 @@
 
 #include <ctype.h>
 #include <stdint.h>
-#include <stdio.h>
+//#include <stdio.h>
 
 #include "../DOS/dos_services_files.h"
 
 uint32_t str_file_count_lines(char* filepath) {
     uint32_t lines = 0;
-    char ch;
+    char chr;
     dos_file_handle_t f = dos_open_file(filepath, ACCESS_READ_ONLY);
     if(!f) {
         return lines;
