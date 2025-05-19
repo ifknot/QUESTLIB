c7a0a27f0772cd92f3ed2722143b5bbd6e358c98
ifknot <ifknot@hotmail.com>
2025-05-19 11:26:32 +0100

    Update str_file.h

--- src/STR/str_file.h
+++ src/STR/str_file.h
@@ -25,12 +25,12 @@ inline str_size_t str_file_read_char(const dos_file_handle_t fhandle, char* chr)
 * @note a word is only counted if starts with a letter, may contain numbers, may be compound (hyphen and/or apostrophe)
 * @note advances the file position pointer to the next word
 */
-str_size_t str_file_read_word(const dos_file_handle_t fhandle, char* string, const str_size_t limit);
+str_size_t str_file_read_word(const dos_file_handle_t fhandle, char* word, const str_size_t limit);
 
 /**
 * @note advances the file position pointer to the start of the next line
 */
-str_size_t str_file_read_line(const dos_file_handle_t fhandle, char* string, const str_size_t limit);
+str_size_t str_file_read_line(const dos_file_handle_t fhandle, char* line, const str_size_t limit);
 
 str_size_t str_file_enumerate_words(const char * path_name, char** string_array, const str_size_t string_limit, const str_size_t array_limit);
 
