fa5f518fc700fd89df1065bf0c798b74b801902b
ifknot <ifknot@hotmail.com>
2025-05-19 12:17:57 +0100

    Update str_constants.h

--- src/STR/str_constants.h
+++ src/STR/str_constants.h
@@ -1,8 +1,6 @@
 #ifndef STR_CONSTANTS_H
 #define STR_CONSTANTS_H
 
-#define STR_MAX_DICTIONARY_WORD_LENGTH 15
-
 static char STR_WHITESPACE[] = " \n\t\v\b\r\f\a";
 
 static char STR_PUNCTUATION[] = ",.;:?!";
