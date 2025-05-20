a08db7d019457e7a421fbeca1eebce68b7f761f9
ifknot <ifknot@hotmail.com>
2025-05-20 21:52:01 +0100

    tdd tweaks

--- src/TDD/tdd_macros.h
+++ src/TDD/tdd_macros.h
@@ -15,40 +15,43 @@
 
 #define FILENAME (strrchr("/" __FILE__, '/') + 1)
 
-#define INTERNAL_ASSERT(expr, assert)                                   \
+#define ASSERT_HALT(expr, halt)                                     \
     do {                                                                \
         if (!(expr)) {                                                  \
             printf("%s:%d - expected: %s\n", FILENAME, __LINE__, #expr);\
             *pass = false;                                              \
-            if (assert)                                                 \
+            if (halt)                                                   \
                 return;                                                 \
         }                                                               \
     } while (0)
 
-#define EXPECT(expr) INTERNAL_ASSERT(expr, 0)
-#define ASSERT(expr) INTERNAL_ASSERT(expr, 1)
+// continues to the next test regardless of the result
+#define EXPECT(expr) ASSERT_HALT(expr, false)
+// returns from the test function if the expression is false
+#define ASSERT(expr) ASSERT_HALT(expr, true)
 
 typedef struct {
-  void (*fn)(bool*);
-  char *name;
+    void (*fn)(bool*);
+    char *name;
 } test_t;
 
-#define TEST(name)                              \
-  static void name##_fn(bool*);                  \
-  static const test_t name = {name##_fn, #name};\
-  static void name##_fn(bool* pass)
+#define TEST(name)                                  \
+    static void name##_fn(bool*);                   \
+    static const test_t name = {name##_fn, #name};  \
+    static void name##_fn(bool* pass)
 
-#define RUN_TESTS(...)                                                      \
-  int run_tests(void) {                                                     \
-    const test_t* tests[] = {__VA_ARGS__};                                  \
-    int failures = 0;                                                       \
-    for (size_t i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {         \
-        bool passed = true;                                                 \
-        tests[i]->fn(&passed);                                              \
-        printf("%s: %s\n", passed ? "PASSED" : "FAILED", tests[i]->name);   \
-        if (!passed) failures++;                                            \
-    }                                                                       \
-    return failures ? EXIT_FAILURE : EXIT_SUCCESS;                          \
-  }
+#define RUN_TESTS(...)                                                          \
+    int run_tests(void) {                                                       \
+        const test_t* tests[] = {__VA_ARGS__};                                  \
+        int failures = 0;                                                       \
+        for (size_t i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {         \
+            bool passed = true;                                                 \
+            tests[i]->fn(&passed);                                              \
+            printf("%s: %s\n", passed ? "PASSED" : "FAILED", tests[i]->name);   \
+            if (!passed)                                                        \
+                failures++;                                                     \
+        }                                                                       \
+        return failures ? EXIT_FAILURE : EXIT_SUCCESS;                          \
+    }
 
 #endif
