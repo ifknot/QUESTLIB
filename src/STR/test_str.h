9feada4e3bffe001005f23885aac6d810dbca9e9
ifknot <ifknot@hotmail.com>
2025-05-21 09:45:24 +0100

    Update test_str.h

--- src/STR/test_str.h
+++ src/STR/test_str.h
@@ -3,14 +3,18 @@
 
 #include "../TDD/tdd_macros.h"
 
-TEST(test_scrubbing) {
-    EXPECT(1 == 0);
-    ASSERT(1 == 1);
-}
+str_size_t str_trim_character(char* string, const char target);
 
-TEST(test_processing) {
-    EXPECT(1 == 1);
-    ASSERT(1 == 0);
-}
+str_size_t str_trim_characters(char* string, const char* targets);
+
+//str_size_t str_copy_trim_characters(const char* const source, destination, const char* targets_array);
+
+str_size_t str_trim_whitespace(char* string)
+
+//str_size_t str_copy_trim_whitespace(const char* source, char* destination);
+
+//str_size_t str_remove_characters(char* source, const char* targets);
+
+str_size_t str_remove_punctuation(char* string);
 
 #endif
