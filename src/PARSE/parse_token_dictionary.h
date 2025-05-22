24195d532d5b0a3dc9a4db2ceca996dad6c76150
ifknot <ifknot@hotmail.com>
2025-05-22 10:14:55 +0100

    Update parse_token_dictionary.h

--- src/PARSE/parse_token_dictionary.h
+++ src/PARSE/parse_token_dictionary.h
@@ -1,4 +1,10 @@
 #ifndef PARSE_TOKE_DICTIONARY_H
 #define PARSE_TOKE_DICTIONARY_H
 
+/**
+* @brief binary search dictionary for zero terminated string containing 1 alpha-numeric word and return its token value, or zero if not found.
+*/
+//parse_token_t parse_tokenize(const parse_word_token_t* dictionary, str_size_t size, const char* target);
+
+
 #endif
