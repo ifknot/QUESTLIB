ec4e737e4d5e80ea9effc45a39208154987a9beb
ifknot <ifknot@hotmail.com>
2025-05-28 16:36:00 +0100

    Update parse_constants.h

--- src/PARSE/parse_constants.h
+++ src/PARSE/parse_constants.h
@@ -4,10 +4,10 @@
 #define PARSE_MAX_LEXEME_LENGTH 15 // compromise for memory usage, any longer lexemes are truncated
 
 #define PARSE_DICTIONARY_NOT_FOUND   -1
-#define PARSE_DICTIONARY_EMPTY       -2
-#define PARSE_DICTIONARY_SORTED      -3
-#define PARSE_DICTIONARY_NOT_SORTED  -4
-#define PARSE_DICTIONARY_DUPLICATE   -5
-
+#define PARSE_DICTIONARY_DUPLICATE   -2
+#define PARSE_DICTIONARY_EMPTY       -3
+#define PARSE_DICTIONARY_FULL        -4
+#define PARSE_DICTIONARY_SORTED      -5
+#define PARSE_DICTIONARY_NOT_SORTED  -6
 
 #endif
