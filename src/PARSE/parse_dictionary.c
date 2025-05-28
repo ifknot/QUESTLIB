c94d875fb67b89106093272526193b02b4334952
ifknot <ifknot@hotmail.com>
2025-05-28 16:04:24 +0100

    Update parse_dictionary.c

--- src/PARSE/parse_dictionary.c
+++ src/PARSE/parse_dictionary.c
@@ -43,22 +43,30 @@ parse_dictionary_t* parse_dictionary_create(mem_arena_t* arena, size_t capacity)
     return dict;
 }
 
-//parse_dictionary_t* parse_dictionary_create_from_file(mem_arena_t* arena, const char * path_name, parse_dictionary_t* dictionary);
+parse_dictionary_t* parse_dictionary_create_from_file(mem_arena_t* arena, const char * path_name, parse_dictionary_t* dictionary) {
 
-void parse_dictionary_add(parse_dictionary_t* dict, char* lexeme, parse_token_t token) {
+}
+
+bool parse_dictionary_add(parse_dictionary_t* dict, char* lexeme, parse_token_t token) {
     assert(dict && lexeme && dict->size < dict->capacity);
-    // TODO use find to disallow duplicates
+    if(parse_dictionary_search(dict, token.lexeme) < 0 {
+        return false;
+    }
     strncpy(dict->pairs[dict->size].lexeme, lexeme, PARSE_MAX_LEXEME_LENGTH);
     dict->pairs[dict->size].lexeme[PARSE_MAX_LEXEME_LENGTH] = '\0'; // strncpy if no null byte among the first n bytes of src, the string placed in dest will not be null-terminated.
     dict->pairs[dict->size].token = token;
     dict->size++;
     dict->is_sorted = false;
+    return true;
 }
 
-void parse_dictionary_remove(parse_dictionary_t* dict, size_t index) {
+bool parse_dictionary_remove(parse_dictionary_t* dict, size_t index) {
     assert(dict && index < dict->size);
-    if(dict->pairs[index].lexeme[0] == '\0') {  // null string
-        return;
+    if(index >= dict->size                           // index out of range 
+        || index < 0                                 // out of rang or passed a search result error
+        || dict->pairs[index].lexeme[0] == '\0')     // indexed entry is a null string
+    { 
+        return false;
     }
     if (index < dict->size - 1) {
         memmove(&dict->pairs[index], &dict->pairs[index + 1], (dict->size - index - 1) * sizeof(parse_lexeme_token_pair_t));
@@ -66,6 +74,7 @@ void parse_dictionary_remove(parse_dictionary_t* dict, size_t index) {
     private_reset_pair(dict, dict->size - 1); // clear last entry
     dict->size--;
     dict->is_sorted = false;
+    return true;
 }
 
 void parse_dictionary_sort(parse_dictionary_t* dict) {
@@ -81,7 +90,7 @@ void parse_dictionary_sort(parse_dictionary_t* dict) {
 *    > 0 if the first string is "greater than" the second.
 * The actual value (not just -1 or 1) is implementation-dependent.
 */
-size_t parse_dictionary_search(const parse_dictionary_t* dict, const char* target) {
+int parse_dictionary_search(const parse_dictionary_t* dict, const char* target) {
     assert(dictionary && target && dict->size && dict->is_sorted);
     str_size_t i = 0; // first entry in dictionary
     str_size_t j = dict->size - 1; // last entry in dictionary
@@ -89,13 +98,17 @@ size_t parse_dictionary_search(const parse_dictionary_t* dict, const char* targe
         int midpoint = i + (j - i) / 2; // calculate new midpoint
         int found = strcmp(dictionary->pairs[midpoint].lexeme, target);
         if (found) {
-            return dictionary[midpoint].token;
+            return midpoint;
         } else if (found < 0) { // target is in the 'upper' half
             i = midpoint + 1;
         } else { // target is in the 'lower' half
             j = midpoint - 1;
         }
     }
+    return -1; // not found
+    //NOT_FOUND -1 
+    //EMPTY -2
+    //SORTED -3
 }
 
 parse_token_t parse_dictionary_at(const parse_dictionary_t* dict, size_t index) {
