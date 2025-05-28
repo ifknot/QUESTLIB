f6c94d440682e91ce18a4dd4be8a9478fc37847d
ifknot <ifknot@hotmail.com>
2025-05-28 15:34:49 +0100

    Update parse_dictionary.h

--- src/PARSE/parse_dictionary.h
+++ src/PARSE/parse_dictionary.h
@@ -41,30 +41,3 @@ bool parse_dictionary_is_sorted(const parse_dictionary_t* dict)
 void parse_dictionary_dump(FILE* output_stream, const parse_dictionary_t* dict);
 
 #endif
-
-
-/*
-* NB The C standard only guarantees that strcmp returns:
-*    < 0 if the first string is "less than" the second.
-*    0 if they are equal.
-*    > 0 if the first string is "greater than" the second.
-* The actual value (not just -1 or 1) is implementation-dependent.
-*/
-/*
-str_token_t str_tokenize(const str_word_token_t* dictionary, str_size_t size, const char* target) { MOVE TO DIC
-    assert(dictionary && target);
-    str_size_t i = 0; // start of dictionary
-    str_size_t j = size - 1; // end of dictionary
-    while (i <= j) {
-        int m = i + (j - i) / 2; // calculate new midpoint
-        int found = strcmp(dictionary[m].word , target);
-        if (found) {
-            return dictionary[m].token;
-        } else if (found < 0) { // target is in the 'upper' half
-            i = m + 1;
-        } else { // target is in the 'lower' half
-            j = m - 1;
-        }
-    }
-    return 0;
-}*/
