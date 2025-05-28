47d9e01a933624bcce6ff6d05a9ab4386f5532d6
ifknot <ifknot@hotmail.com>
2025-05-28 15:35:13 +0100

    Update parse_dictionary.h

--- src/PARSE/parse_dictionary.h
+++ src/PARSE/parse_dictionary.h
@@ -13,7 +13,7 @@ typedef struct private_parse_dictionary parse_dictionary_t;
 
 parse_dictionary_t* parse_dictionary_create(mem_arena_t* arena, size_t size);
 
-//parse_dictionary_t* parse_dictionary_create_from_file(mem_arena_t* arena, const char * path_name, parse_dictionary_t* dictionary);
+parse_dictionary_t* parse_dictionary_create_from_file(mem_arena_t* arena, const char * path_name, parse_dictionary_t* dictionary);
 
 void parse_dictionary_add(parse_dictionary_t* dict, char* lexeme, parse_token_t token);
 
