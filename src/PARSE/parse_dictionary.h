f1e350f2124124a5812e435b492c8e4e4dc429da
ifknot <ifknot@hotmail.com>
2025-05-28 15:00:43 +0100

    Update parse_dictionary.h

--- src/PARSE/parse_dictionary.h
+++ src/PARSE/parse_dictionary.h
@@ -22,7 +22,7 @@ void parse_dictionary_remove(parse_dictionary_t* dict, size_t index);
 void parse_dictionary_sort(parse_dictionary_t* dict);
 
 /*
-Binary search for token index *requires* sorted dictionary 
+Binary search for token index *requires* sorted dictionary and assert fails if not
 */
 size_t parse_dictionary_find(const parse_dictionary_t* dict, const char* lexeme);
 
