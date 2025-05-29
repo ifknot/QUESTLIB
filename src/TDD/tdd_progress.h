f8205aa2bd7dd0868eb9c5af228b898724a714c6
ifknot <ifknot@hotmail.com>
2025-05-29 11:34:26 +0100

    Update tdd_progress.h

--- src/TDD/tdd_progress.h
+++ src/TDD/tdd_progress.h
@@ -7,18 +7,9 @@
 #ifndef TDD_PROGRESS_H
 #define TDD_PROGRESS_H
 
-#include <stdio.h>
-#include <time.h>
-
 /* ----------------- Progress Tracking ----------------- */
 
-typedef struct {
-    size_t total;
-    size_t current;
-    size_t width;       // bar width - 0 if not a width widget
-    size_t step;        // step size for percent or rotation - if 0 step is calculated
-} tdd_progress_t;
-
+typedef struct private_tdd_progress_t tdd_progress_t;
 
 tdd_progress_t tdd_progress_start(size_t total, size_t current, size_t step, size_t width);
 
