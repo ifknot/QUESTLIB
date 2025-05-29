17b72e5919a07803c46190eb153b1e13f7aa22fd
ifknot <ifknot@hotmail.com>
2025-05-29 13:59:54 +0100

    Create tdd_constants.h

---
+++ src/TDD/tdd_constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS.H
+#define CONSTANTS.H
+
+// classical ascii spinner highly portable
+#define TDD_SPINNER_1_CHARS  "|/-\\"
+
+//CP437 IBM XT pulsing light 
+#define TDD_SPINNER_2_CHARS  "\176\177\178\219\178\177"
+
+#endif
