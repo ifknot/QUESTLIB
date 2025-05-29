1f5bbfe3f675a9d11da396a5599273019e8dc85d
ifknot <ifknot@hotmail.com>
2025-05-29 14:06:20 +0100

    Update tdd_constants.h

--- src/TDD/tdd_constants.h
+++ src/TDD/tdd_constants.h
@@ -4,7 +4,8 @@
 // classical ascii spinner highly portable
 #define TDD_SPINNER_DEFAULT_CHARS  "|/-\\"
 
-//CP437 IBM XT pulsing box
+// CP437 IBM XT pulsing box
+// @url https://en.wikipedia.org/wiki/Code_page_437
 #define TDD_SPINNER_CP437_CHARS  "\176\177\178\219\178\177"
 
 #endif
