c1c9457c2a7357bd9d57154172f95022d6e6d4fb
ifknot <ifknot@hotmail.com>
2025-05-29 12:10:05 +0100

    Update tdd_spinner.c

--- src/TDD/tdd_spinner.c
+++ src/TDD/tdd_spinner.c
@@ -3,10 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    size_t counter;
+    const char* spinner_frames;
+} private_tdd_spinner_t;
+
 tdd_spinner_t tdd_spinner_start(const char* frames) {
     tdd_spinner_t spin;
     spin.counter = 0;
-    spin.spinner_frames = frames ? frames : "|/-\\";
+    spin.spinner_frames = (frames) ? frames : "|/-\\";
     return spin;
 }
 
