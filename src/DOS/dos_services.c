92da2633e8644cd0fd9f71e9fa424bffcf80b4b5
ifknot <ifknot@hotmail.com>
2025-05-30 17:37:41 +0100

    Update dos_services.c

--- src/DOS/dos_services.c
+++ src/DOS/dos_services.c
@@ -1,3 +1,10 @@
+/**
+ * @defgroup dos_services DOS Interrupt Services
+ * @brief Collection of functions for DOS interrupt services
+ * @details Provides safe wrappers around fundamental DOS interrupt services
+ * for memory management and interrupt vector manipulation.
+ * @{
+ */
 #include "dos_services.h"
 
 #include <stdio.h>
@@ -7,13 +14,20 @@
 #include "dos_error_messages.h"
 
 /**
-* @brief Provides a safe method for changing interrupt vectors
-* @note returns nothing
-* INT 21,25 - Set Interrupt Vector
-* AH = 25h
-* AL = interrupt number
-* DS:DX = pointer to interrupt handler
-*/
+ * @brief Provides a safe method for changing interrupt vectors
+ * @details Uses INT 21h, AH=25h to set an interrupt vector.
+ * @note This operation is critical and should be used carefully as it modifies system interrupt table.
+ * 
+ * @param vec_num The interrupt vector number to modify (0-255)
+ * @param phandler Pointer to the new interrupt handler function
+ * 
+ * @asm
+ *   INT 21,25 - Set Interrupt Vector
+ *   AH = 25h
+ *   AL = interrupt number
+ *   DS:DX = pointer to interrupt handler
+ * @endasm
+ */
 void dos_set_interrupt_vector(uint8_t vec_num, void* phandler) {
     __asm {
         .8086
@@ -31,16 +45,20 @@ void dos_set_interrupt_vector(uint8_t vec_num, void* phandler) {
 }
 
 /**
-* @brief Standard method for retrieving interrupt vectors
-* INT 21,35 - Get Interrupt Vector
-* AH = 35h
-* AL = interrupt vector number
-* on return:
-* ES:BX = pointer to interrupt handler.
-*
-* @param vec_num
-* @return void* segment:offset pointer to interrupt handler
-*/
+ * @brief Retrieves the current interrupt vector
+ * @details Uses INT 21h, AH=35h to get an interrupt vector address.
+ * 
+ * @param vec_num The interrupt vector number to query (0-255)
+ * @return void* Segment:offset pointer to current interrupt handler (little-endian format)
+ * 
+ * @asm
+ *   INT 21,35 - Get Interrupt Vector
+ *   AH = 35h
+ *   AL = interrupt vector number
+ *   Returns:
+ *   ES:BX = pointer to interrupt handler
+ * @endasm
+ */
 void* dos_get_interrupt_vector(uint8_t vec_num) {
     void* phandler = 0;
     __asm {
@@ -62,39 +80,30 @@ void* dos_get_interrupt_vector(uint8_t vec_num) {
 }
 
 /**
-* @brief INT 21,48 - Allocate Memory
-* Allocates a specified number of memory paragraphs.
-*
-* AH = 48h
-* BX = number of memory paragraphs requested
-* @note paragraph is a memory unit of size 16 bytes,  relevant primarily (if not exclusively) in x86 real mode
-* @url https://en.wikipedia.org/wiki/X86_memory_segmentation
-* on return:
-* AX = segment address of allocated memory block (MCB + 1para)
-*    = error code if CF set  (see DOS ERROR CODES)
-* BX = size in paras of the largest block of memory available
-*      if CF set, and AX = 08 (Not Enough Mem)
-* CF = 0 if successful
-*    = 1 if error
-*
-* Error codes:   7          Memory control blocks destroyed
-*                8          Insufficient memory
-* @note 1. Call Function 59h for extended error code information (DOS 3.0 and above).
-*
-* - returns segment address of allocated memory block AX:0000
-* - each allocation requires a 16 byte overhead for the MCB
-* - returns maximum block size available if error
-*
-* @note 2. By setting BX=FFFFh before calling, this function can be used to find the amount of
-* available memory, which will be returned in BX. (The call will return an error, which can be
-* ignored, since DOS cannot allocate more than 640k of memory.)
-*
-* @see  INT 21,49,  INT 21,4A
-*
-* @param       number of paragraphs (16 bytes) requested
-* @param       segment* pointer to segment variable
-* @return      the segment address of the reserved memory or 0 if request failed
-*/
+ * @brief Allocates memory blocks in paragraphs
+ * @details Uses INT 21h, AH=48h to allocate memory in 16-byte paragraphs.
+ * @warning Each allocation requires a 16-byte overhead for the Memory Control Block (MCB).
+ * 
+ * @param paragraphs Number of 16-byte paragraphs requested
+ * @return uint16_t Segment address of allocated block (AX:0000) or 0 if failed
+ * 
+ * @asm
+ *   INT 21,48 - Allocate Memory
+ *   AH = 48h
+ *   BX = number of memory paragraphs requested
+ *   Returns:
+ *   AX = segment address of allocated block (MCB + 1 para) if successful
+ *      = error code if CF set
+ *   BX = size of largest available block if AX=8 (insufficient memory)
+ *   CF = 0 if success, 1 if error
+ * @endasm
+ * 
+ * @retval 0 Allocation failed
+ * @retval >0 Segment address of allocated memory
+ * 
+ * @note To find available memory size, set BX=FFFFh before calling (will return error but BX contains available memory)
+ * @see dos_free_allocated_memory_blocks()
+ */
 uint16_t dos_allocate_memory_blocks(uint16_t paragraphs) {
     uint16_t available, mem_seg;
     dos_error_code_t err_code;
@@ -128,24 +137,29 @@ OK: mov     mem_seg, ax
 }
 
 /**
-* @brief INT 21,49 - Free Allocated Memory
-*
-* AH = 49h
-* ES = segment of the block to be returned (MCB + 1para)
-*
-* on return:
-* AX = error code if CF set  (see DOS ERROR CODES)
-*
-* - releases memory and MCB allocated by INT 21,48
-* - may cause unpredictable results if memory wasn't allocated using INT 21,48
-* or if memory wasn't allocated by the current process
-* - checks for valid MCB id, but does NOT check for process ownership care must be
-* taken when freeing the memory of another process to assure the segment isn't in use
-* by a TSR or ISR
-* @note this function is unreliable in a TSR once resident, since COMMAND.COM and many
-*  other .COM files take all available memory when they load.
-* @see  INT 21,4A
-*/
+ * @brief Frees previously allocated memory blocks
+ * @details Uses INT 21h, AH=49h to release memory allocated by dos_allocate_memory_blocks().
+ * @warning Unpredictable results if memory wasn't allocated by current process or wasn't allocated properly.
+ * 
+ * @param segment Segment address of memory to free (MCB + 1 paragraph)
+ * @return uint16_t DOS error code (0 if success)
+ * 
+ * @asm
+ *   INT 21,49 - Free Allocated Memory
+ *   AH = 49h
+ *   ES = segment of block to free (MCB + 1 para)
+ *   Returns:
+ *   AX = error code if CF set
+ *   CF = 0 if success, 1 if error
+ * @endasm
+ * 
+ * @retval 0 Success
+ * @retval 7 Memory control blocks destroyed
+ * @retval 8 Insufficient memory (shouldn't occur for free operation)
+ * 
+ * @note Unreliable in TSR programs once resident, as COMMAND.COM and others take all memory when loading.
+ * @see dos_allocate_memory_blocks()
+ */
 uint16_t dos_free_allocated_memory_blocks(uint16_t segment) {
     dos_error_code_t err_code = 0;
     __asm {
@@ -170,3 +184,5 @@ uint16_t dos_free_allocated_memory_blocks(uint16_t segment) {
 #endif
     return err_code;
 }
+
+/** @} */ // end of dos_services group
