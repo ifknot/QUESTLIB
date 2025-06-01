4b049d1da974afd32a8a7d82498ec0a1ec645338
ifknot <ifknot@hotmail.com>
2025-06-01 21:12:54 +0100

    Update parse_constants.h

--- src/PARSE/parse_constants.h
+++ src/PARSE/parse_constants.h
@@ -1,14 +1,33 @@
+/**
+ * @file parse_constants.h
+ * @brief Constants for parsing and dictionary operations
+ * @defgroup parse_constants Parsing Constants
+ * @{
+ */
 #ifndef PARSE_CONSTANTS_H
 #define PARSE_CONSTANTS_H
 
-#define PARSE_MAX_LEXEME_LENGTH 15 // compromise for memory usage, any longer lexemes are truncated
+/**
+ * @brief Maximum length for lexemes in characters
+ * @details Longer lexemes will be truncated to this length.
+ * @note Includes space for null terminator (actual max content is PARSE_MAX_LEXEME_LENGTH-1)
+ */
+#define PARSE_MAX_LEXEME_LENGTH 15
 
-#define PARSE_DICTIONARY_NOT_FOUND      -1
-#define PARSE_DICTIONARY_DUPLICATE      -2
-#define PARSE_DICTIONARY_EMPTY          -3
-#define PARSE_DICTIONARY_FULL           -4
-#define PARSE_DICTIONARY_SORTED         -5
-#define PARSE_DICTIONARY_NOT_SORTED     -6
-#define PARSE_DICTIONARY_OUT_OF_BOUNDS  -7
+/**
+ * @name Dictionary Status Codes
+ * @brief Return values for dictionary operations
+ * @{
+ */
+#define PARSE_DICTIONARY_NOT_FOUND      -1  /**< Lexeme not found in dictionary */
+#define PARSE_DICTIONARY_DUPLICATE      -2  /**< Lexeme already exists in dictionary */
+#define PARSE_DICTIONARY_EMPTY          -3  /**< Dictionary contains no entries */
+#define PARSE_DICTIONARY_FULL           -4  /**< Dictionary has reached capacity */
+#define PARSE_DICTIONARY_SORTED         -5  /**< Dictionary is sorted (unused in current implementation) */
+#define PARSE_DICTIONARY_NOT_SORTED     -6  /**< Dictionary needs sorting for this operation */
+#define PARSE_DICTIONARY_OUT_OF_BOUNDS  -7  /**< Index parameter is invalid */
+/** @} */
 
 #endif
+
+/** @} */ // End of parse_constants group
