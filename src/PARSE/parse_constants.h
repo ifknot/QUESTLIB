0ee32a2271089f74615a884011ae60af87c39a00
ifknot <ifknot@hotmail.com>
2025-06-02 12:01:24 +0100

    Update parse_constants.h

--- src/PARSE/parse_constants.h
+++ src/PARSE/parse_constants.h
@@ -14,20 +14,6 @@
  */
 #define PARSE_MAX_LEXEME_LENGTH 15
 
-/**
- * @name Dictionary Status Codes
- * @brief Return values for dictionary operations
- * @{
- */
-#define PARSE_DICTIONARY_NOT_FOUND      -1  /**< Lexeme not found in dictionary */
-#define PARSE_DICTIONARY_DUPLICATE      -2  /**< Lexeme already exists in dictionary */
-#define PARSE_DICTIONARY_EMPTY          -3  /**< Dictionary contains no entries */
-#define PARSE_DICTIONARY_FULL           -4  /**< Dictionary has reached capacity */
-#define PARSE_DICTIONARY_SORTED         -5  /**< Dictionary is sorted (unused in current implementation) */
-#define PARSE_DICTIONARY_NOT_SORTED     -6  /**< Dictionary needs sorting for this operation */
-#define PARSE_DICTIONARY_OUT_OF_BOUNDS  -7  /**< Index parameter is invalid */
-/** @} */
-
 #endif
 
 /** @} */ // End of parse_constants group
