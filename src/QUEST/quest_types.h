32e6d785a7407f2dd49be87d30036de46bc13236
ifknot <ifknot@hotmail.com>
2025-06-04 16:27:44 +0100

    Update quest_types.h

--- src/QUEST/quest_types.h
+++ src/QUEST/quest_types.h
@@ -24,18 +24,15 @@ typedef struct { // D&D 5e, the six ability scores (Strength, Dexterity, Constit
     char charisma; 
 } quest_character_core_t;
 
-typedef struct {
-    char type;    // class Abilities eg fighter ability to use a battle axe, while a wizard able to cast spells.
+typedef struct {   //  a character is an item with character information 
+    quest_item_t* item;    // id class Abilities eg fighter ability to use a battle axe, while a wizard able to cast spells.
     char life;    // reduced by the cost of living increased by food fluid and manna spells
-    char*  brief;
-    char*  details;
     quest_character_skills_t* skills[8]; // upto eight skills can be added and enhanced or reduced 
     quest_item_t* items[8];    // upto 8 items, such as sword, chest, spell, bread etc etc
     quest_character_core_t core;
 } quest_character_t;    // e.g. Troll, Gaurd, 
 
-typedef struct {
-    int id;
+typedef struct { // a player is a character with player information
     quest_character_t* character;
     int items_max;
 } quest_player_t;
