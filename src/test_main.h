fac39e19ad6004ccca769015e34e214e1a6f492c
ifknot <ifknot@hotmail.com>
2025-06-04 10:51:28 +0100

    Create test_main.h

---
+++ src/test_main.h
@@ -0,0 +1,39 @@
+#ifndef TEST_MAIN_H
+#define TEST_MAIN_H
+
+
+#include "TDD/test_tdd.h"
+#include "DOS/test_dos_services.h"
+#include "STR/test_str.h"
+#include "FILE/test_file.h"
+#include "MEM/test_mem_tools.h"
+#include "MEM/test_mem_arena.h"
+#include "PARSE/test_parse_dictionary.h"
+
+RUN_TESTS( /*TDD_TESTS,
+    DOS_SERVICES_TESTS,
+    FILE_TESTS,
+    STR_TESTS,
+    TOOLS_TESTS,
+    ARENA_TESTS*/
+    DICTIONARY_TESTS
+)
+
+#endif
+
+/**
+ * Usage:
+ * @code 
+ *
+ * int main(int argc, char** argv) {
+ *
+ * #ifndef __LARGE__
+ *    printf("Incorrect memory model is selected.\n");
+ *    printf("Rebuild RETROLIB using the large memory model with the -ml compiler option.\n");
+ *    return 0;
+ * #endif
+ *
+ *   return run_tests();
+ *
+ * }
+ */
