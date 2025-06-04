45cbe786d7fb9b74449fcb2f0fd295ae50023eb1
ifknot <ifknot@hotmail.com>
2025-06-04 15:38:28 +0100

    Update quest_types.h

--- src/QUEST/quest_types.h
+++ src/QUEST/quest_types.h
@@ -5,9 +5,13 @@ typedef struct {
     int    id;              
     char*  brief;
     char*  details;
+    quest_item_t* items[8];  // pointers to upto 8 items - within this item
+} quest_item_t;
+
+typedef struct {    // a location is an item with location info
+    quest_item_t* item;
     char   exits;            // 8 bits of direction options |N|E|S|W|U|D|L|R| north, south, east, west, up, down, left, right 1 if exit valid 0 otherwise 
     int    destination[8];   // 8 location IDs associated with each valid exit 
-    quest_item_t* items[8];  // pointers to upto 10 items 
     quest_dictionary_t* local_dict; // parse dictionary specific to this room additional to the global dictionary (to avoid users spamming the input with verbs and nouns to cheat on the story)
 } quest_location_t;
 
@@ -18,17 +22,22 @@ typedef struct { // D&D 5e, the six ability scores (Strength, Dexterity, Constit
     char intelligence;
     char wisdom;
     char charisma; 
-} quest_player_core_t;
+} quest_character_core_t;
 
 typedef struct {
-    
-} quest_character_t;
+    char type;    // class Abilities eg fighter ability to use a battle axe, while a wizard able to cast spells.
+    char life;    // reduced by the cost of living increased by food fluid and manna spells
+    char*  brief;
+    char*  details;
+    quest_character_skills_t* skills[8]; // upto eight skills can be added and enhanced or reduced 
+    quest_item_t* items[8];    // upto 8 items, such as sword, chest, spell, bread etc etc
+    quest_character_core_t core;
+} quest_character_t;    // e.g. Troll, Gaurd, 
 
 typedef struct {
     int id;
     quest_character_t* character;
     int items_max;
-    quest_item_t* items[8];
 } quest_player_t;
 
 #endif
