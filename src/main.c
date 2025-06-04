3bf02123b5fbc64a6c9e7f61a9217d2d1d8832d4
ifknot <ifknot@hotmail.com>
2025-06-04 10:58:10 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,32 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
-#include "TDD/test_tdd.h"
-#include "DOS/test_dos_services.h"
-#include "STR/test_str.h"
-#include "FILE/test_file.h"
-#include "MEM/test_mem_tools.h"
-#include "MEM/test_mem_arena.h"
-*/
-#include "PARSE/test_parse_dictionary.h"
+quest_t quest;
 
-RUN_TESTS( /*TDD_TESTS,
-    DOS_SERVICES_TESTS,
-    FILE_TESTS,
-    STR_TESTS,
-    TOOLS_TESTS,
-    ARENA_TESTS*/
-    DICTIONARY_TESTS
-)
+quest_create(&quest, "Tower Quest", global_dictionary);
+
+quest_location_t* start = quest_add_location(&quest, "info", container, local_dict)
 
 int main(int argc, char** argv) {
 
-#ifndef __LARGE__
-    printf("Incorrect memory model is selected.\n");
-    printf("Rebuild RETROLIB using the large memory model with the -ml compiler option.\n");
-    return 0;
-#endif
-
-    return run_tests();
+    return EXIT_SUCCESS;
 
 }
