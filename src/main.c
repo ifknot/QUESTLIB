2dc14e192d40b054a6ed5c27581d498ba8e399cc
ifknot <ifknot@hotmail.com>
2025-06-04 11:37:45 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-quest_t quest;
+mem_arena_t* quest_arena = mem_arena_new(MEM_ARENA_POLICY_DOS, MEM_SIZE_1K);
 
-quest_create(&quest, "Tower Quest", global_dictionary);
+parse_dictionary_t* global_dictionary parse_dictionary_create_from_file(quest_arena , "TOWER.DIC");
 
-quest_location_t* start = quest_add_location(&quest, "info", container, local_dict)
+location_dicts[] load_location_dicts("LOCALS.DIC");
+
+location_info[] load_location_strings("LOCALS.INF");
+
+quest_create(&quest, "Tower Quest", &global_dictionary);
+
+quest_location_t* start = quest_add_location(&quest, location_info[0], container, location_dicts[0])
 
 int main(int argc, char** argv) {
 
