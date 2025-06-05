7e7f5790cf2af8d9cc4c7526e7c0628e147d2609
ifknot <ifknot@hotmail.com>
2025-06-05 08:54:33 +0100

    Update quest_uid.h

--- src/QUEST/quest_uid.h
+++ src/QUEST/quest_uid.h
@@ -1,17 +1,8 @@
-
-
+#ifndef QUEST_UID_H
+#define QUEST_UID_H
 
 typedef struct private_quest_uid_t quest_uid_t;
 
-/* in imp file
-typedef struct {
-    quest_size_t uid
-} private_quest_uid;
-
-static quest_uid_t quest_global_uid;
-
-*/
-
-void quest_uid_reset();
-
 quest_size_t quest_next_uid();
+
+#endif
