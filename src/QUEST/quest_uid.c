ca2294df315c477bcbc568fdeb0af5c1764c4e8b
ifknot <ifknot@hotmail.com>
2025-06-05 09:48:24 +0100

    Update quest_uid.c

--- src/QUEST/quest_uid.c
+++ src/QUEST/quest_uid.c
@@ -1,15 +1,41 @@
-#include "quest_uid.h" 
-
+/**
+ * @file quest_uid.c
+ * @brief Unique ID Generator Implementation
+ *
+ * Implements a simple sequential unique ID generator.
+ * The internal counter starts at 0 and increments with each call.
+ */
+#include "quest_uid.h"
 #include "quest_types.h"
 
-typedef struct {
+/**
+ * @brief Internal state structure for the UID generator
+ *
+ * Contains the current counter value that gets incremented
+ * on each call to quest_next_uid().
+ */
+struct quest_uid_t {
+    quest_size_t i;  /**< Current counter value */
+};
 
-    quest_size_t i;
+/**
+ * @brief Static instance of the UID generator state
+ *
+ * Initialized to zero at program startup. 
+ * Maintains persistence between calls to quest_next_uid().
+ */
+static struct quest_uid_t uid = { 0 };
 
-} private_quest_uid_t;
-
-static quest_uid_t uid = { 0 };
-
-quest_size_t quest_next_uid() {
-    return  uid.i++;
+/**
+ * @brief Implementation of quest_next_uid()
+ *
+ * @return The next available unique identifier
+ *
+ * @details
+ * This implementation simply increments and returns a counter.
+ * @note The counter starts at 0 
+ * @warning Counter will wrap around when reaching the maximum value of quest_size_t.
+ */
+quest_size_t quest_next_uid(void) {
+    return uid.i++;
 }
