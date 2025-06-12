1187d08a37589edc2622956aa60e8fc22aaf0006
ifknot <ifknot@hotmail.com>
2025-06-12 15:03:30 +0100

    Update quest_door.c

--- src/QUEST/quest_door.c
+++ src/QUEST/quest_door.c
@@ -1,19 +1,13 @@
 #include "quest_door.h"
 #include <assert.h>
 
-void quest_door_init(
+void quest_door_init(    // defaults to an open door with no lock
     quest_door_t* door,
-    quest_location_t* loc1,
-    quest_location_t* loc2,
-    quest_type_t type,
+     quest_type_t type,
     quest_info_t* info,
-    bool locked,
-    quest_rtti_t key
-) { 
-    quest_connector_init(&door->base, loc1, loc2, type, info);
-    door->is_locked = locked;
-    door->key = key;
-}
+    quest_location_t* loc1,
+    quest_location_t* loc2
+);
 
 quest_door_t* quest_door_create(
     mem_arena_t* arena,
@@ -23,37 +17,13 @@ quest_door_t* quest_door_create(
     quest_info_t* info,
     bool locked,
     quest_rtti_t key
-) {
-    assert(arena && "NULL arena");
-    assert(loc1 && loc2 && "NULL locations");
-    assert(info && "NULL info");
-    
-    quest_door_t* door = mem_arena_alloc(arena, sizeof(quest_door_t));
-    assert(door && "NULL door!");
-    
-    quest_door_init(door, loc1, loc2, type, info, locked, key);
-    
-    return door;
-}
+);
 
-quest_error_t quest_door_lock(quest_door_t* door) {
-    assert(door && "NULL door");
+quest_error_t quest_door_open(quest_door_t* door);
 
-    if (door->is_locked) {
-        return QUEST_ALREADY_LOCKED;
-    }
+quest_error_t quest_door_close(quest_door_t* door);
 
-    door->is_locked = true;
-    return QUEST_SUCCESS;
-}
+quest_error_t quest_door_lock(quest_door_t* door, quest_key_t* key);
 
-quest_error_t quest_door_unlock(quest_door_t* door, const quest_rtti_t key) {
-    assert(door && "NULL door");
+quest_error_t quest_door_unlock(quest_door_t* door, quest_key_t* key);
 
-    if (door->is_locked) {
-        return QUEST_ALREADY_LOCKED;
-    }
-
-    door->is_locked = true;
-    return QUEST_SUCCESS;
-}
