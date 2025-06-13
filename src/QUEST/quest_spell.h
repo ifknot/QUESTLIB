14a276073f0ccebf5753725917c728fb51021d0d
ifknot <ifknot@hotmail.com>
2025-06-13 09:59:20 +0100

    Create quest_spell.h

---
+++ src/QUEST/quest_spell.h
@@ -0,0 +1,14 @@
+#ifndef QUEST_SPELL_H
+#define QUEST_SPELL_H
+
+#include "quest_types_h"
+
+typedef struct {
+  quest_size_t spell;
+} quest_spell_t;
+
+typedef struct {
+  quest_size_t undo;
+} quest_undo_spell_t;
+
+#endif
