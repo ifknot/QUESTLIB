b65582fae8dbd5fac338ea55363a8b1519a785e4
ifknot <ifknot@hotmail.com>
2025-06-18 20:56:16 +0100

    stat into its own lib

--- src/STAT/stat_describe.h
+++
@@ -1,8 +0,0 @@
-#ifndef STAT_DESCRIBE_H
-#define STAT_DESCRIBE_H
-
-#include "stat_types.h"
-
-stat_descriptive_stats_t stat_describe(stat_float_t* data, stat_size_t size);
-
-#endif // STAT_DESCRIBE_H
